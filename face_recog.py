6b7829bba3be4290ad5e4060f04cc7c439f7842b
Manu M B <mbmanu74@gmail.com>
2021-06-25 13:57:55 +0530

    updated codes

--- face_recog.py
+++ face_recog.py
@@ -1,6 +1,11 @@
 import cv2
 import numpy as np
 import os 
+import datetime
+import requests
+data = {}
+temp = None
+url = 'https://6b662ece687a.ngrok.io/v0/nodelog'
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read('trainer/trainer.yml')
@@ -13,7 +18,7 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 id = 0
 
 
-names = ['None', 'Mustafa','Shujan','Manu'] 
+names = ['Unknown', 'Mustafa M C','Shujan Pannag J','Manu M B'] 
 
 # Initialize and start realtime video capture
 cam = cv2.VideoCapture(0)
@@ -29,19 +34,14 @@ while True:
     #img = cv2.flip(img, -1) # Flip vertically
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     
-    faces = faceCascade.detectMultiScale( 
-        gray,
-        scaleFactor = 1.2,
-        minNeighbors = 5,
-        minSize = (int(minW), int(minH)),
-       )
+    faces = faceCascade.detectMultiScale(gray, scaleFactor = 1.2, minNeighbors = 5, minSize = (int(minW), int(minH)),)
 
     for(x,y,w,h) in faces:
         cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
         id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
 
         # Check if confidence is less them 100 ==> "0" is perfect match 
-        if (confidence < 100):
+        if (confidence < 70):
             id = names[id]
             confidence = "  {0}%".format(round(100 - confidence))
         else:
@@ -49,7 +49,18 @@ while True:
             confidence = "  {0}%".format(round(100 - confidence))
         
         cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
-        cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
+        cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1) 
+
+
+        if temp != str(id):
+            temp = str(id)
+            data['relname'] = str(id)
+            data['ipaddr'] = requests.get('https://api.ipify.org/').text
+            data['datetime'] = str(datetime.datetime.now())[0:19]
+            x = requests.post(url = url , json = data)
+            print(x.text)
+            temp = str(id)
+
     
     cv2.imshow('camera',img) 
 
